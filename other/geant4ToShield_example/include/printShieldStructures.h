fb869dce83f0bff02d3b54d3953a2f0006a01b08
kazymai <kazymai@jinr.ru>
2015-05-05 14:49:16 +0300

    before

---
+++ other/geant4ToShield_example/include/printShieldStructures.h
@@ -0,0 +1,13 @@
+#ifndef PRINT_SHIELD_STRUCTURES_H
+#define PRINT_SHIELD_STRUCTURES_H
+
+#include "shield.h"
+#include <stdio.h>
+
+void printElement(Element element);
+void printMediumData(MediumData medium);
+void printSGeoBody(SGeoBody body);
+void printSGeoZone(SGeoZone zone);
+
+
+#endif
